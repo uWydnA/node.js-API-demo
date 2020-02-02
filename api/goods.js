7dd8e83a5d1667f219eb72c017ac14000f406f55
uWydnA <40652024+uWydnA@users.noreply.github.com>
2020-02-02 13:33:49 +0800

    bug-fx

--- api/goods.js
+++ api/goods.js
@@ -2,11 +2,10 @@
         * @api {get} /api/goods 
         * @apiDescription 某电商网站商品列表数据的接口
         * @apiName 商品列表数据
-        * @apiParam {String} a=hello
         * @apiSuccess {json} data 请求成功后的数据
         * @apiSuccessExample {json} Success-Response:
         * // 返回数据示例
-        * { code:"200", msg:"商品列表数据", length:数据长度, data:data }
+        * { code:"100", msg:"商品列表数据", length:数据长度, data:data }
         * @apiSampleRequest http://localhost:3000/api/goods
         * @apiVersion 1.0.0
         */
@@ -20,7 +19,7 @@ router.get('/', function (req, res, next) {
     sql.find({
         colName: coll
     }).then(data=>{
-        res.send( { code:"200", msg:"商品列表数据", length:data.length, data:data });
+        res.send( { code:"100", msg:"商品列表数据", length:data.length, data:data });
     },err=>{
         res.send({ code:"444", msg:"NOT DATA" })
     })
